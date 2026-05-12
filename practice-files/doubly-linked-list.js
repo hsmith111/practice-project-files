87f9bfb1f1092bbf424d4fc7ca43c283da7f8628
Heidi Smith <heidi.smith@ramseysolutions.com>
2026-05-12 11:46:20 -0500

    Add examples to test functionality

--- practice-files/doubly-linked-list.js
+++ practice-files/doubly-linked-list.js
@@ -100,6 +100,22 @@ class DoublyLinkedList {
     }
 }
 const subway = new DoublyLinkedList()
+subway.addToHead('TimesSquare');
+subway.addToHead('GrandCentral');
+subway.addToHead('CentralPark');
+subway.printList();
+
+subway.addToTail('PennStation');
+subway.addToTail('WallStreet');
+subway.addToTail('BrooklynBridge');
+subway.printList();
+
+subway.removeHead();
+subway.removeTail();
+subway.printList();
+
+subway.removeByData('TimesSquare');
+subway.printList();
 
 
 module.exports = DoublyLinkedList;
